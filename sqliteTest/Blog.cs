9af4231ef6701d78148c7ff5471f65c8d75dda2f
Renier Valdes <rvaldes@Reniers-MacBook-Pro.local>
2019-11-20 00:34:51 -0500

    add solution

---
+++ sqliteTest/Blog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace sqliteTest
+{
+    /// <summary>
+    /// Blog entity
+    /// </summary>
+    public class Blog
+    {
+        [Key]
+        public int BlogId { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string SubTitle { get; set; }
+
+        [Required]
+        public DateTime DateTimeAdd { get; set; }
+    }
+}
---
+++ sqliteTest/MyDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace sqliteTest
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=TestDatabase.db", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Map table names
+            modelBuilder.Entity<Blog>().ToTable("Blogs", "test");
+            modelBuilder.Entity<Blog>(entity =>
+            {
+                entity.HasKey(e => e.BlogId);
+                entity.HasIndex(e => e.Title).IsUnique();
+                entity.Property(e => e.DateTimeAdd).HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
